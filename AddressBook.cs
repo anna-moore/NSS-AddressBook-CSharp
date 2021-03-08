1cb5e2af636cecef434203ca08c73d0a0eb07252
anna-moore <ammoore434@gmail.com>
2021-03-08 11:36:47 -0600

    first commit

---
+++ AddressBook.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    public class book
+    {
+        public Dictionary<string, Contact> Person { get; set; }
+        // = new Dictionary<string, string>();
+        // public List<Person> CurrentContacts { get; set; }
+    }
+
+}
